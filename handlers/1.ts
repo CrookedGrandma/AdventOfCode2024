7bc0a8ccb2b7042591831abc107919b1aabb01c9
CrookedGrandma <k.t.noot@live.nl>
2024-12-02 14:00:59 +0100

    Add day 1

--- handlers/1.ts
+++ handlers/1.ts
@@ -1,11 +1,31 @@
 import {Handler} from "../handler";
+import { sum } from "../util";
 
 export class H1 extends Handler {
+    private left: number[] = [];
+    private right: number[] = [];
+    private dist: number[] = [];
+
     runA(input: string[]): Output {
-        return 0;
+        for (const line of input) {
+            const [l, r] = line.split(/\s+/);
+            this.left.push(+l);
+            this.right.push(+r);
+        }
+        this.left.sort((a, b) => a - b);
+        this.right.sort((a, b) => a - b);
+        for (let i = 0; i < this.left.length; i++) {
+            this.dist.push(Math.abs(this.left[i] - this.right[i]));
+        }
+        return sum(this.dist);
     }
 
     runB(input: string[]): Output | undefined {
-        return;
+        const scores = [];
+        for (const l of this.left) {
+            const count = this.right.filter(r => r === l).length;
+            scores.push(l * count);
+        }
+        return sum(scores);
     }
 }
